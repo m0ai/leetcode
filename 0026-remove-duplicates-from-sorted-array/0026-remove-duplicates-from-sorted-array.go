ae8e8664f811bfc32934b498f18e12d9e4121042
Choi Yeongjun <frozenmoai@gmail.com>
2023-10-09 03:28:08 +0900

    Time: 5 ms (88.73%), Space: 4.4 MB (18.79%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
@@ -1,11 +1,11 @@
 func removeDuplicates(nums []int) int {
-  var leastNum, k int = nums[0], 1
+  var lastNum, k int = nums[0], 1
 
   for i := 1; i < len(nums); i++ { 
-      if nums[i] > leastNum {
-        leastNum = nums[i]
-        nums[k] = leastNum
-        k += 1
+      if nums[i] > lastNum {
+        lastNum = nums[i]
+        nums[k] = lastNum
+        k ++
       }
       continue
   }
