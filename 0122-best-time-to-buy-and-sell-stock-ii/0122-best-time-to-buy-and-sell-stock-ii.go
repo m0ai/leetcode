fc7b7f00416593b4793f475358d2c7a83059c426
Choi Yeongjun <frozenmoai@gmail.com>
2023-10-24 01:21:12 +0900

    Time: 5 ms (67.61%), Space: 3 MB (100%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,27 +1,34 @@
 func maxProfit(prices []int) int {
     profit := 0 
-    boughtPrice := 0
-    hasStock := false
-    for day, price := range prices {
-        // when end of days
-        // sell
-        if hasStock && boughtPrice < price  {
-            profit += price
-            boughtPrice = -1
-            hasStock = false
-        }
 
-        if day == len(prices)-1 && !hasStock {
-            continue
-        }
-        
-        // buy
-        if !hasStock && price < prices[day+1] {
-            profit -= price
-            boughtPrice = price
-            hasStock = true
+    for i := 0; i < len(prices)-1; i ++ {
+        if (prices[i] < prices[i+1]){
+            profit += prices[i+1]-prices[i]
+
         }
     }
     return profit
+
+    // for day, price := range prices {
+    //     // when end of days
+    //     // sell
+    //     if hasStock && boughtPrice < price  {
+    //         profit += price
+    //         boughtPrice = -1
+    //         hasStock = false
+    //     }
+
+    //     if day == len(prices)-1 && !hasStock {
+    //         continue
+    //     }
+
+    //     // buy
+    //     if !hasStock && price < prices[day+1] {
+    //         profit -= price
+    //         boughtPrice = price
+    //         hasStock = true
+    //     }
+    // }
+    // return profit
 }
  
